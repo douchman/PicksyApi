af81194ea302d15389141561d9d8a3683c581f63
buck <take6948@naver.com>
2024-12-30 18:38:47 +0900

    :sparkles: security context 로 부터 인증된 유저의 식별자와 로그인 id 추출 메서드 추가 #30

---
+++ vsplay/src/main/java/com/buck/vsplay/global/security/service/IAuthUserService.java
@@ -0,0 +1,6 @@
+package com.buck.vsplay.global.security.service;
+
+public interface IAuthUserService {
+    Long getAuthUserId();
+    String getAuthUserLoginId();
+}
--- vsplay/src/main/java/com/buck/vsplay/global/security/service/impl/IAuthUserService.java
+++
@@ -1,8 +0,0 @@
-package com.buck.vsplay.global.security.service.impl;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface IAuthUserService {
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
