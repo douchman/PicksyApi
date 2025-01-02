f1c63388f6fc089ec4def80f504ca971bcc4d170
buck <take6948@naver.com>
2025-01-02 14:39:58 +0900

    :recycle: Member Entity 조회 메서드 추가 #32

--- vsplay/src/main/java/com/buck/vsplay/global/security/service/impl/AuthUserService.java
+++ vsplay/src/main/java/com/buck/vsplay/global/security/service/impl/AuthUserService.java
@@ -1,8 +1,11 @@
 package com.buck.vsplay.global.security.service.impl;
 
+import com.buck.vsplay.domain.member.entity.Member;
+import com.buck.vsplay.domain.member.repository.MemberRepository;
 import com.buck.vsplay.global.security.service.IAuthUserService;
 import com.buck.vsplay.global.security.user.CustomUserDetail;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -10,7 +13,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthUserService implements IAuthUserService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Member getAuthUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetail customUserDetail)) {
+            throw new IllegalStateException("JWT Token is missing in the authentication context");
+        }
+
+        return memberRepository.findById(customUserDetail.getId())
+                .orElseThrow( () -> new IllegalStateException("Member not found"));
+    }
 
     @Override
     public String getAuthUserLoginId() {
