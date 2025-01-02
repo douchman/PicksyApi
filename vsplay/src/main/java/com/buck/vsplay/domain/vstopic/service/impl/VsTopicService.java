e5b28735eb827c1efc0b97a898a7b6b45cd9b984
buck <take6948@naver.com>
2025-01-02 14:40:33 +0900

    :recycle: Member 매핑 후 삽입 처리 #32

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/impl/VsTopicService.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/impl/VsTopicService.java
@@ -1,9 +1,12 @@
 package com.buck.vsplay.domain.vstopic.service.impl;
 
+import com.buck.vsplay.domain.member.entity.Member;
 import com.buck.vsplay.domain.vstopic.dto.VsTopicDto;
+import com.buck.vsplay.domain.vstopic.entity.VsTopic;
 import com.buck.vsplay.domain.vstopic.mapper.VsTopicMapper;
 import com.buck.vsplay.domain.vstopic.repository.VsTopicRepository;
 import com.buck.vsplay.domain.vstopic.service.IVsTopicService;
+import com.buck.vsplay.global.security.service.impl.AuthUserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,10 +19,13 @@ import org.springframework.stereotype.Service;
 public class VsTopicService implements IVsTopicService {
     private final VsTopicRepository vsTopicRepository;
     private final VsTopicMapper vsTopicMapper;
+    private final AuthUserService authUserService;
 
     @Override
     public void createVsTopic(VsTopicDto.VsTopicCreateRequest createVsTopicRequest) {
-        log.info("created VsTopic Request -> {}", createVsTopicRequest);
-        vsTopicRepository.save(vsTopicMapper.toEntity(createVsTopicRequest));
+        Member existMember = authUserService.getAuthUser();
+        VsTopic vsTopic = vsTopicMapper.toEntity(createVsTopicRequest);
+        vsTopic.setMember(existMember);
+        vsTopicRepository.save(vsTopic);
     }
 }
