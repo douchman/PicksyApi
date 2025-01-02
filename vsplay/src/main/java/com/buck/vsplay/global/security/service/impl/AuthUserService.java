278c6ad33792b0549c06013d858681ab99057882
buck <take6948@naver.com>
2025-01-02 16:13:18 +0900

    :fire: AuthUserService 불필요 메서드 제거 #38

--- vsplay/src/main/java/com/buck/vsplay/global/security/service/impl/AuthUserService.java
+++ vsplay/src/main/java/com/buck/vsplay/global/security/service/impl/AuthUserService.java
@@ -3,7 +3,6 @@ package com.buck.vsplay.global.security.service.impl;
 import com.buck.vsplay.domain.member.entity.Member;
 import com.buck.vsplay.domain.member.repository.MemberRepository;
 import com.buck.vsplay.global.security.service.IAuthUserService;
-import com.buck.vsplay.global.security.user.CustomUserDetail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -27,23 +26,5 @@ public class AuthUserService implements IAuthUserService {
 
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalStateException("Member not found"));
-    }
-
-    @Override
-    public String getAuthUserLoginId() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetail customUserDetail)) {
-            throw new IllegalStateException("Authentication is invalid or not set");
-        }
-        return customUserDetail.getUsername();
-    }
-
-    @Override
-    public Long getAuthUserId() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetail customUserDetail)) {
-            throw new IllegalStateException("JWT Token is missing in the authentication context");
-        }
-        return customUserDetail.getId();
     }
 }
