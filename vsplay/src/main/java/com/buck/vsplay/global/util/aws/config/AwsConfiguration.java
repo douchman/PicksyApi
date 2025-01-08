3008a25f1897e1cb74eb820dfeb929d846d8cf23
buck <take6948@naver.com>
2025-01-08 14:00:14 +0900

    :sparkles: AwsConfiguration 추가 #56

---
+++ vsplay/src/main/java/com/buck/vsplay/global/util/aws/config/AwsConfiguration.java
@@ -0,0 +1,38 @@
+package com.buck.vsplay.global.util.aws.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+public class AwsConfiguration {
+
+    @Bean
+    public S3Presigner s3Presigner(
+            @Value("${aws.credentials.access-key}") String accessKey,
+            @Value("${aws.credentials.secret-key}") String secretKey) {
+        return S3Presigner.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey))
+                ).build();
+    }
+
+    @Bean
+    public S3Client s3Client(
+            @Value("${aws.credentials.access-key}") String accessKey,
+            @Value("${aws.credentials.secret-key}") String secretKey){
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey))
+                ).build();
+
+    }
+}
