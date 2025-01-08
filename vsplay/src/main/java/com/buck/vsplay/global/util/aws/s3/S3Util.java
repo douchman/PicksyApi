880da557eee2aa209d0bbb24ccecb67add691651
buck <take6948@naver.com>
2025-01-08 14:03:51 +0900

    :sparkles: S3Util 추가 추가 #56

---
+++ vsplay/src/main/java/com/buck/vsplay/global/util/aws/s3/S3Util.java
@@ -0,0 +1,64 @@
+package com.buck.vsplay.global.util.aws.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+
+import java.io.IOException;
+import java.time.Duration;
+
+@Service
+public class S3Util {
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    private final S3Presigner s3Presigner;
+    private final S3Client s3Client;
+
+    public S3Util(S3Presigner s3Presigner, S3Client s3Client) {
+        this.s3Presigner = s3Presigner;
+        this.s3Client = s3Client;
+    }
+
+    public String getUploadedObjectUrl(String objectKey){
+        // S3 GetObject
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .build();
+
+        // PreSigned URL
+        GetObjectPresignRequest preSignRequest = GetObjectPresignRequest.builder()
+                .getObjectRequest(getObjectRequest)
+                .signatureDuration(Duration.ofSeconds(30))
+                .build();
+
+        return s3Presigner.presignGetObject(preSignRequest).url().toString();
+    }
+
+    public String putObject(MultipartFile file) throws IOException {
+        String objectKey = file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .contentDisposition(file.getContentType())
+                .build();
+
+        s3Client.putObject(
+                putObjectRequest,
+                RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        return objectKey;
+    }
+}
