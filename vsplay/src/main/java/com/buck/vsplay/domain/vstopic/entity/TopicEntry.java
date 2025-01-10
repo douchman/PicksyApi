3b462497a7805bbe1412d67caa6edd9d33994716
buck <take6948@naver.com>
2025-01-10 13:56:15 +0900

    :memo: TopicEntry Entity 코멘트 추가 #66

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/entity/TopicEntry.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/entity/TopicEntry.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Comment;
 
 @Entity
 @NoArgsConstructor
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @Table(name = "TOPIC_ENTRY")
 @SequenceGenerator(name ="ENTRY_SEQ_GENERATOR", sequenceName = "ENTRY_SEQ")
+@Comment("주제에 소속되는 대결 아이템(엔트리)")
 public class TopicEntry extends Timestamp {
 
     @Id
@@ -25,13 +27,16 @@ public class TopicEntry extends Timestamp {
     private VsTopic topic;
 
     @Column(name = "entry_name", nullable = false, length = 30)
+    @Comment("엔트리 이름")
     private String entryName;
 
     @Column(name = "description", nullable = false, length = 200)
+    @Comment("엔트리 설명")
     private String description;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "media_type", nullable = false)
+    @Comment("엔트리 미디어 타입 (사진, 영상링크 등 )")
     private MediaType mediaType;
 
     public enum MediaType{
