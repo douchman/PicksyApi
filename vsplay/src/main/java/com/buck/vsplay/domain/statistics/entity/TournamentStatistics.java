c853bebd7150093fa33f2f37610e6fe75d4302cd
buck <take6948@naver.com>
2025-01-13 13:02:44 +0900

    :sparkles: TournamentStatistics 엔티티 추가 #70

---
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/entity/TournamentStatistics.java
@@ -0,0 +1,37 @@
+package com.buck.vsplay.domain.statistics.entity;
+
+
+import com.buck.vsplay.domain.vstopic.entity.VsTopic;
+import com.buck.vsplay.global.entity.Timestamp;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "TOURNAMENT_STATISTICS")
+@Getter
+@Setter
+@NoArgsConstructor
+@SequenceGenerator(name = "TOURNAMENT_STATS_SEQ_GENERATOR" , sequenceName = "TOURNAMENT_STATS_SEQ")
+@Comment("토너먼트 분석 데이터")
+public class TournamentStatistics extends Timestamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TOURNAMENT_STATS_SEQ_GENERATOR")
+    private Long tournamentStatsId;
+
+    @ManyToOne( fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name ="topic_id")
+    private VsTopic vsTopic;
+
+    @Column(name = "tournament_stage", unique = true)
+    @Comment("토너먼트 단계 (ex. 8강 16강")
+    private Integer tournamentStage;
+
+    @Column(name = "stage_matches")
+    @Comment("진행 횟수")
+    private Integer stageMatches;
+
+}
