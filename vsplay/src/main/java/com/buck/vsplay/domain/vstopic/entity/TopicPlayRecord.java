c8331b63a54a23af80e5315cd5db4d9c6084f7e2
buck <take6948@naver.com>
2025-01-14 14:11:38 +0900

    :recycle: Member fetch 타입 변경 #77

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/entity/TopicPlayRecord.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/entity/TopicPlayRecord.java
@@ -24,7 +24,7 @@ public class TopicPlayRecord extends Timestamp {
     @JoinColumn(name = "topic_id")
     private VsTopic topic;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
