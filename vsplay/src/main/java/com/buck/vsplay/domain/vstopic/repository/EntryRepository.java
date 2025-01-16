581c2322cf48d26d5830c77fb4df95aa64b6e6ca
buck <take6948@naver.com>
2025-01-16 16:24:50 +0900

    :sparkles: 주제에 소속된 엔트리 리스트를 대결주제 식별자로 조회하는 쿼리메서드 추가 #102

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/repository/EntryRepository.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/repository/EntryRepository.java
@@ -2,8 +2,14 @@ package com.buck.vsplay.domain.vstopic.repository;
 
 import com.buck.vsplay.domain.vstopic.entity.TopicEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EntryRepository extends JpaRepository<TopicEntry, Long> {
+
+    @Query("SELECT e FROM TopicEntry e WHERE e.topic.id = :topicId")
+    List<TopicEntry> findByTopicId(Long topicId);
 }
