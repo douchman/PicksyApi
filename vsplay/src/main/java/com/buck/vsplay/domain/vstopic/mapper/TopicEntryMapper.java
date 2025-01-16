4b5bfd6cf309a120fc017a34c3c963cda7dc3d85
buck <take6948@naver.com>
2025-01-16 16:22:28 +0900

    :recycle: 추가된 dto 매핑 추가 #102 - 업로드 파일 경로를 S3Util 을 이용해 매핑 단계에서 preSignedUrl 로 반환 하도록 단일객체 매핑 작성 - 이후 List 매핑을 작성하여 해당 단일객체 매핑을 사용하도록 정의

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/mapper/TopicEntryMapper.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/mapper/TopicEntryMapper.java
@@ -4,10 +4,13 @@ package com.buck.vsplay.domain.vstopic.mapper;
 import com.buck.vsplay.domain.vstopic.dto.EntryDto;
 import com.buck.vsplay.domain.vstopic.entity.TopicEntry;
 import com.buck.vsplay.domain.vstopic.entity.VsTopic;
+import com.buck.vsplay.global.util.aws.s3.S3Util;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TopicEntryMapper {
@@ -15,4 +18,14 @@ public interface TopicEntryMapper {
 
     @Mapping(target = "topic", expression = "java(vsTopic)")
     TopicEntry toTopicEntryWithTopic(EntryDto.Entry topicEntry, @Context VsTopic vsTopic);
+
+    @Mapping(source = "id", target ="entryId")
+    @Mapping(target = "mediaUrl" , expression = "java(s3Util.getUploadedObjectUrl(topicEntry.getMediaUrl()))")
+    EntryDto.CreatedEntry toCreatedEntry(TopicEntry topicEntry, @Context S3Util s3Util);
+
+    default List<EntryDto.CreatedEntry> toCreatedEntryDtoList(List<TopicEntry> entries, @Context S3Util s3Util){
+        return entries.stream()
+                .map(entry -> toCreatedEntry(entry, s3Util))
+                .toList();
+    }
 }
