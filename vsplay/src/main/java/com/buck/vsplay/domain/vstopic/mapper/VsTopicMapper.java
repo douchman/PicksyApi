77131dacadb8d8af4b9cdaffb8148bcfcdf9950d
buck <take6948@naver.com>
2025-01-16 14:39:02 +0900

    :fire: 불필요한 코드 제거 #92

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/mapper/VsTopicMapper.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/mapper/VsTopicMapper.java
@@ -4,15 +4,11 @@ package com.buck.vsplay.domain.vstopic.mapper;
 import com.buck.vsplay.domain.vstopic.dto.VsTopicDto;
 import com.buck.vsplay.domain.vstopic.entity.VsTopic;
 import org.mapstruct.*;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface VsTopicMapper {
-    VsTopicMapper INSTANCE = Mappers.getMapper(VsTopicMapper.class);
-    VsTopic toEntity(VsTopicDto.VsTopic vsTopic);
     @Mapping(target = "thumbnail", ignore = true)
     VsTopic toEntity(VsTopicDto.VsTopicCreateRequest vsTopicCreateRequest);
-    VsTopicDto.VsTopic toDto(VsTopic vsTopic);
 
     @Mapping(target = "thumbnail", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
