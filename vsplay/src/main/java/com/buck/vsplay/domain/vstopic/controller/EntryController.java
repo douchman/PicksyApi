cad6446e0ba995bdce4019361724479e6ef7e9fe
buck <take6948@naver.com>
2025-01-16 16:26:10 +0900

    :sparkles: 주제에 포함된 엔트리 리스트 조회 API 추가 102

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/controller/EntryController.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/controller/EntryController.java
@@ -7,15 +7,21 @@ import com.buck.vsplay.global.dto.SingleResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class EntryController {
 
     private final EntryService entryService;
+
+    @GetMapping("vstopic/entries/{id}")
+    public ResponseEntity<SingleResponseDto<EntryDto.CreatedEntryList>> getAllEntries(
+            @PathVariable("id") Long topicId
+    ) {
+
+        return new ResponseEntity<>(new SingleResponseDto<>(HttpStatus.OK.value(), entryService.getEntriesByTopicId(topicId)) ,HttpStatus.OK);
+    }
 
     @PostMapping("vstopic/entries")
     public ResponseEntity<SingleResponseDto<Integer>> createEntries(
