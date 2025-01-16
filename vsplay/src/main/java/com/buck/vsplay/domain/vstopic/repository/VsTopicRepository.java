5113446cefe287a165d47308dae9c3ace681f105
buck <take6948@naver.com>
2025-01-16 14:01:55 +0900

    :bug: JpaRepository 제네릭 파라미터 Long 으로 변경 #93

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/repository/VsTopicRepository.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/repository/VsTopicRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VsTopicRepository extends JpaRepository<VsTopic, Integer> {
+public interface VsTopicRepository extends JpaRepository<VsTopic, Long> {
 }
