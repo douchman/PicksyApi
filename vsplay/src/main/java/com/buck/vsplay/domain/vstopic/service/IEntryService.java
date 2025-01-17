2661e7cdbd7e46f7512e2863f55a6d67fa351eb5
buck <take6948@naver.com>
2025-01-17 14:40:03 +0900

    :sparkles: 엔트리 업데이트 기능 구현 #106

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/IEntryService.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/IEntryService.java
@@ -5,4 +5,5 @@ import com.buck.vsplay.domain.vstopic.dto.EntryDto;
 public interface IEntryService {
     EntryDto.CreatedEntryList getEntriesByTopicId(Long topicId);
     void createEntries(Long topicId, EntryDto.CreateEntriesRequest request);
+    void updateEntries(Long topicId, Long entryId, EntryDto.UpdateEntryRequest updatedRequest);
 }
