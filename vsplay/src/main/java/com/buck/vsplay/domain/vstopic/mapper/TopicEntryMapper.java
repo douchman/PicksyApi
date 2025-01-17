d63d95235d3fe15999a99eea2d2987b8e6e061a1
buck <take6948@naver.com>
2025-01-17 14:39:35 +0900

    :sparkles: 엔트리 업데이트 요청 값을 기준으로 변경된 값을 entity 에 적용하는 매핑메서드 작성 #106 - 설명을 위한 javadoc 을 추가하였음

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/mapper/TopicEntryMapper.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/mapper/TopicEntryMapper.java
@@ -5,9 +5,7 @@ import com.buck.vsplay.domain.vstopic.dto.EntryDto;
 import com.buck.vsplay.domain.vstopic.entity.TopicEntry;
 import com.buck.vsplay.domain.vstopic.entity.VsTopic;
 import com.buck.vsplay.global.util.aws.s3.S3Util;
-import org.mapstruct.Context;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 import java.util.List;
 
@@ -26,4 +24,14 @@ public interface TopicEntryMapper {
                 .map(entry -> toCreatedEntry(entry, s3Util))
                 .toList();
     }
+
+    /**
+     * 엔티티 업데이트를 위해 DTO 의 값을 기존 Entry 엔티티에 적용
+     * Null 필드는 자동으로 제외
+     * @param updatedRequest  기존 엔트리에 업데이트 할 변경된 엔트리 값
+     * @param topicEntry 업데이트를 적용할 기존의 엔트리
+     */
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void applyChangesToTopicEntry(EntryDto.UpdateEntryRequest updatedRequest, @MappingTarget TopicEntry topicEntry);
+
 }
