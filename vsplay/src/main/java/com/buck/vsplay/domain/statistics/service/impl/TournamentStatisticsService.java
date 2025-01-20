d704e02d3c6b5a8fc0d2a5138c3b18197a4ef001
buck <take6948@naver.com>
2025-01-20 16:33:25 +0900

    :sparkles: 토너먼트 업데이트 기능 구현 및 이벤트 리스너 추가 #114

---
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/service/impl/TournamentStatisticsService.java
@@ -0,0 +1,35 @@
+package com.buck.vsplay.domain.statistics.service.impl;
+
+import com.buck.vsplay.domain.statistics.entity.TournamentStatistics;
+import com.buck.vsplay.domain.statistics.event.TournamentUpdateEvent;
+import com.buck.vsplay.domain.statistics.repository.TournamentStatisticsRepository;
+import com.buck.vsplay.domain.statistics.service.ITournamentStatisticsService;
+import com.buck.vsplay.domain.vstopic.entity.TopicTournament;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TournamentStatisticsService implements ITournamentStatisticsService {
+
+    private final TournamentStatisticsRepository tournamentStatisticsRepository;
+
+    @EventListener // 생성된 토너먼트 엔티티에 대한 이벤트 구독
+    public void handleTopicTournamentUpdated(TournamentUpdateEvent tournamentUpdateEvent){
+
+        updateTournamentStatistics(tournamentUpdateEvent.getTopicTournament());
+    }
+
+    @Override
+    public void updateTournamentStatistics(TopicTournament topicTournament) {
+        log.info(" updateTournamentStatistics 메서드 실행");
+        tournamentStatisticsRepository.save(TournamentStatistics.builder()
+                .topicTournament(topicTournament)
+                .tournamentStage(topicTournament.getTournamentStage())
+                .build());
+
+    }
+}
