11e99bd0c3d938acde03f30ccf7a16c08ec15665
buck <take6948@naver.com>
2025-01-21 17:26:40 +0900

    :sparkle: 진행불가능한 토너먼트 예외코드 추가 #122

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/exception/tournament/TournamentExceptionCode.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/exception/tournament/TournamentExceptionCode.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 @Getter
 public enum TournamentExceptionCode implements BaseExceptionCode {
 
-    SERVER_ERROR(500, "토너먼트 처리 중 서버 오류가 발생했습니다.", "TOURNAMENT_001");
+    SERVER_ERROR(500, "토너먼트 처리 중 서버 오류가 발생했습니다.", "TOURNAMENT_001"),
+    TOURNAMENT_INVALID(400, "진행할 수 없는 토너먼트 입니다.", "TOURNAMENT_002"),
+    ;
 
     private final Integer status;
     private final String message;
