7683cfb70d08708134208264e17572d02aebfb00
buck <take6948@naver.com>
2025-01-21 14:32:53 +0900

    :recycle: Tournament dto, 토너먼트상세정보 반환 dto 추가 #121

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/dto/VsTopicDto.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/dto/VsTopicDto.java
@@ -4,10 +4,11 @@ package com.buck.vsplay.domain.vstopic.dto;
 import com.buck.vsplay.global.constants.Visibility;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Data
@@ -40,6 +41,20 @@ public class VsTopicDto {
         private String description;
         private MultipartFile thumbnail;
         private Visibility visibility;
+    }
+
+    @Data
+    public static class Tournament{
+        Long id;
+        Integer tournamentStage;
+        String tournamentName;
+    }
+
+    @Data
+    @ToString
+    public static class VsTopicDetailWithTournamentsResponse{
+        VsTopic topic;
+        List<Tournament> tournamentList = new ArrayList<>();
     }
 
 }
