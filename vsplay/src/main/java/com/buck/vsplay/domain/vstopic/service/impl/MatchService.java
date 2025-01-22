70e740ef6360584c903bbc7903e0ad91a19d2215
buck <take6948@naver.com>
2025-01-22 13:39:37 +0900

    :sparkles: 대진표 초기화 기능 추가 #127 - 첫 대진표 초기화 메서드 추가 - 대결진행기록 생성 시 해당 대진표 초기화 수행

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/impl/MatchService.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/impl/MatchService.java
@@ -1,21 +1,23 @@
 package com.buck.vsplay.domain.vstopic.service.impl;
 
 import com.buck.vsplay.domain.vstopic.dto.TopicPlayRecordDto;
+import com.buck.vsplay.domain.vstopic.entity.EntryMatch;
+import com.buck.vsplay.domain.vstopic.entity.TopicEntry;
 import com.buck.vsplay.domain.vstopic.entity.TopicPlayRecord;
 import com.buck.vsplay.domain.vstopic.entity.VsTopic;
+import com.buck.vsplay.domain.vstopic.exception.playrecord.PlayRecordException;
 import com.buck.vsplay.domain.vstopic.exception.tournament.TournamentException;
 import com.buck.vsplay.domain.vstopic.exception.tournament.TournamentExceptionCode;
 import com.buck.vsplay.domain.vstopic.exception.vstopic.VsTopicException;
 import com.buck.vsplay.domain.vstopic.exception.vstopic.VsTopicExceptionCode;
-import com.buck.vsplay.domain.vstopic.repository.TopicPlayRecordRepository;
-import com.buck.vsplay.domain.vstopic.repository.TournamentRepository;
-import com.buck.vsplay.domain.vstopic.repository.VsTopicRepository;
+import com.buck.vsplay.domain.vstopic.repository.*;
 import com.buck.vsplay.domain.vstopic.service.IMatchService;
 import com.buck.vsplay.global.constants.PlayStatus;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import java.util.*;
 
 
 @Service
@@ -25,26 +27,63 @@ import org.springframework.stereotype.Service;
  public class MatchService implements IMatchService {
 
     private final VsTopicRepository vsTopicRepository;
+    private final EntryRepository entryRepository;
     private final TopicPlayRecordRepository topicPlayRecordRepository;
     private final TournamentRepository tournamentRepository;
+    private final EntryMatchRepository entryMatchRepository;
 
     @Override
     public TopicPlayRecordDto.PlayRecordResponse createTopicPlayRecord(Long topicId, TopicPlayRecordDto.PlayRecordRequest playRecordRequest) {
+        try {
+            VsTopic topic = vsTopicRepository.findById(topicId).orElseThrow(
+                    () -> new VsTopicException(VsTopicExceptionCode.TOPIC_NOT_FOUND));
 
-        VsTopic topic = vsTopicRepository.findById(topicId).orElseThrow(
-                () -> new VsTopicException(VsTopicExceptionCode.TOPIC_NOT_FOUND));
+            if (!isTournamentExist(topic, playRecordRequest.getTournamentStage())){
+                throw new TournamentException(TournamentExceptionCode.TOURNAMENT_INVALID);
+            }
 
-        if (!isTournamentExist(topic, playRecordRequest.getTournamentStage())){
-            throw new TournamentException(TournamentExceptionCode.TOURNAMENT_INVALID);
+            TopicPlayRecord savedTopicPlayRecord = topicPlayRecordRepository.save(TopicPlayRecord.builder()
+                    .topic(topic)
+                    .tournament(playRecordRequest.getTournamentStage())
+                    .status(PlayStatus.IN_PROGRESS)
+                    .build());
+
+            initializeFirstTournament(savedTopicPlayRecord); // 대결 진행 기록 후 첫 대진표 생성
+
+            return new TopicPlayRecordDto.PlayRecordResponse(savedTopicPlayRecord.getId());
+
+        }catch (PlayRecordException e) {
+            log.error("토너먼트 대진표 초기화 중 오류가 발생했습니다", e);
+            throw e;
         }
+    }
 
-        TopicPlayRecord savedTopicPlayRecord = topicPlayRecordRepository.save(TopicPlayRecord.builder()
-                .topic(topic)
-                .tournament(playRecordRequest.getTournamentStage())
-                .status(PlayStatus.IN_PROGRESS)
-                .build());
+    /**
+     * 대진표가 전혀 없는 상태에서 가장 첫 토너먼트 대진표 생성
+     * @param topicPlayRecord 대결진행기록
+     */
+    private void initializeFirstTournament(TopicPlayRecord topicPlayRecord) throws PlayRecordException {
 
-        return new TopicPlayRecordDto.PlayRecordResponse(savedTopicPlayRecord.getId());
+        List<TopicEntry> entryList = entryRepository.findByTopicId(topicPlayRecord.getTopic().getId());
+        Collections.shuffle(entryList); // 무작위 순서 셔플
+
+
+        entryList = entryList.subList(0, topicPlayRecord.getTournament()); // 셔플 후 토너먼트 진행에 필요한 최대 엔트리 갯수만큼 자르기
+
+        while (!entryList.isEmpty()) {
+            TopicEntry entryA = entryList.get(0); // 순서대로 엔트리 조회
+            TopicEntry entryB = entryList.get(1);
+            entryMatchRepository.save(EntryMatch.builder()
+                    .topicPlayRecord(topicPlayRecord)
+                    .entryA(entryA)
+                    .entryB(entryB)
+                    .tournamentRound(topicPlayRecord.getTournament())
+                    .status(PlayStatus.IN_PROGRESS)
+                    .build());
+
+            entryList.remove(entryA); // 매칭이 완료된 엔트리는 리스트에서 제거
+            entryList.remove(entryB);
+        }
     }
 
     private boolean isTournamentExist(VsTopic vsTopic, int tournamentStage) {
