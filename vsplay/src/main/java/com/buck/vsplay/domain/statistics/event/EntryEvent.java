7a412a2a956f8a09b85c5227b447c712e8fe4070
buck <take6948@naver.com>
2025-01-29 16:25:44 +0900

    :sparkles: 엔트리 관련 이벤트 추가 #176 - 생성타입 이벤트 정의

---
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/event/EntryEvent.java
@@ -0,0 +1,19 @@
+package com.buck.vsplay.domain.statistics.event;
+
+import com.buck.vsplay.domain.vstopic.entity.TopicEntry;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EntryEvent {
+
+    @Data
+    @AllArgsConstructor
+    public static class CreateEvent{
+        List<TopicEntry> topicEntryList;
+    }
+}
