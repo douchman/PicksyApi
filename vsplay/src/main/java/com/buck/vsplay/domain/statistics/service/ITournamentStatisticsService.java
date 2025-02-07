26796aa67724191e5ff02318ccef2da25c869933
buck <take6948@naver.com>
2025-02-07 17:10:40 +0900

    :sparkles: 대결주제의 토너먼트 이용 통계 조회 #234

--- vsplay/src/main/java/com/buck/vsplay/domain/statistics/service/ITournamentStatisticsService.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/service/ITournamentStatisticsService.java
@@ -1,9 +1,12 @@
 package com.buck.vsplay.domain.statistics.service;
 
 
+import com.buck.vsplay.domain.statistics.dto.TournamentStatisticsDto;
 import com.buck.vsplay.domain.vstopic.entity.TopicTournament;
 
 public interface ITournamentStatisticsService {
     void createTournamentStatistics(TopicTournament topicTournament);
     void recordMatchStat(TopicTournament topicTournament);
+
+    TournamentStatisticsDto.TournamentStatisticsResponse getTournamentStatistics(Long topicId);
 }
