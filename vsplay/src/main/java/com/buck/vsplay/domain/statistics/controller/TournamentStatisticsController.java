62a14c1a57b80b53e838e10bdfb9ab6f9df72712
buck <take6948@naver.com>
2025-02-07 17:11:14 +0900

    :sparkles: 대결주제 토너먼트 이용 통계 조회 요청 엔드포인트 추가 #234

---
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/controller/TournamentStatisticsController.java
@@ -0,0 +1,27 @@
+package com.buck.vsplay.domain.statistics.controller;
+
+
+import com.buck.vsplay.domain.statistics.dto.TournamentStatisticsDto;
+import com.buck.vsplay.domain.statistics.service.impl.TournamentStatisticsService;
+import com.buck.vsplay.global.dto.SingleResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("statistics/vstopic")
+public class TournamentStatisticsController {
+
+    private final TournamentStatisticsService tournamentStatisticsService;
+
+    @GetMapping("{topicId}/tournament")
+    public ResponseEntity<SingleResponseDto<TournamentStatisticsDto.TournamentStatisticsResponse>> getTournamentStatistics(@PathVariable("topicId") Long topicId) {
+
+        return new ResponseEntity<>(new SingleResponseDto<>(HttpStatus.OK.value(), tournamentStatisticsService.getTournamentStatistics(topicId)), HttpStatus.OK);
+    }
+}
