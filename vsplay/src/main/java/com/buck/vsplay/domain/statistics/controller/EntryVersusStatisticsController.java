17b85b922c3d739c81ecff84e48eeed2f9067e2a
buck <take6948@naver.com>
2025-02-11 14:53:42 +0900

    :sparkles: 엔트리 대결 상성 통계 엔드포인트 추가 #238

---
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/controller/EntryVersusStatisticsController.java
@@ -0,0 +1,32 @@
+package com.buck.vsplay.domain.statistics.controller;
+
+
+import com.buck.vsplay.domain.statistics.dto.EntryVersusStatisticsDto;
+import com.buck.vsplay.domain.statistics.service.impl.EntryVersusStatisticsService;
+import com.buck.vsplay.global.dto.SingleResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("statistics/versus/vstopic/")
+public class EntryVersusStatisticsController {
+
+    private final EntryVersusStatisticsService entryVersusStatisticsService;
+
+    @GetMapping("{topicId}/entries/{entryId}")
+    public ResponseEntity<SingleResponseDto<List<EntryVersusStatisticsDto.EntryVersusStatistics>>> getEntryVersusStatistics(
+            @PathVariable("topicId") Long topicId,
+            @PathVariable("entryId") Long entryId){
+
+        return new ResponseEntity<>(new SingleResponseDto<>(HttpStatus.OK.value(), entryVersusStatisticsService.getEntryVersusStatistics(topicId, entryId)), HttpStatus.OK);
+
+    }
+}
