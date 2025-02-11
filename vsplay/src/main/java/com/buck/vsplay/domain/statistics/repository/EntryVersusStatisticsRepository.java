f404c37ff6b24ebd195ca710ecf044f39741875e
buck <take6948@naver.com>
2025-02-11 14:50:45 +0900

    :sparkles: 엔트리 상성 통계 조회 쿼리 메서드 추가 #238

--- vsplay/src/main/java/com/buck/vsplay/domain/statistics/repository/EntryVersusStatisticsRepository.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/repository/EntryVersusStatisticsRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EntryVersusStatisticsRepository extends JpaRepository<EntryVersusStatistics, Long> {
 
     @Query("SELECT evs FROM EntryVersusStatistics evs WHERE evs.topicEntry.id = :entryId AND evs.opponentEntry.id = :opponentEntryId")
     EntryVersusStatistics findByEntryIdAndOpponentEntryId(@Param("entryId") Long entryId, @Param("opponentEntryId") Long opponentEntryId);
+
+    @Query("SELECT evs FROM EntryVersusStatistics evs JOIN FETCH evs.opponentEntry oe WHERE evs.topicEntry.id = :entryId")
+    List<EntryVersusStatistics> findWithOpponentEntryByEntryId(@Param("entryId") Long entryId);
 }
