30bc36cd38e749198173287084639601dc029c74
buck <take6948@naver.com>
2025-02-14 14:48:04 +0900

    :sparkles: Optional 반환 메서드를 추가 #266

--- vsplay/src/main/java/com/buck/vsplay/global/security/service/impl/AuthUserService.java
+++ vsplay/src/main/java/com/buck/vsplay/global/security/service/impl/AuthUserService.java
@@ -1,8 +1,6 @@
 package com.buck.vsplay.global.security.service.impl;
 
 import com.buck.vsplay.domain.member.entity.Member;
-import com.buck.vsplay.domain.member.exception.MemberException;
-import com.buck.vsplay.domain.member.exception.MemberExceptionCode;
 import com.buck.vsplay.domain.member.repository.MemberRepository;
 import com.buck.vsplay.global.security.service.IAuthUserService;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -20,13 +20,21 @@ public class AuthUserService implements IAuthUserService {
     private final MemberRepository memberRepository;
 
     @Override
-    public Member getAuthUser() {
+    public Optional<Member> getAuthUserOptional() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 인증 정보가 없거나 principal이 Long 타입이 아니면 비로그인
         if (authentication == null || !(authentication.getPrincipal() instanceof Long memberId)) {
-            throw new IllegalStateException("JWT Token is missing in the authentication context");
+            return Optional.empty();
         }
 
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new MemberException(MemberExceptionCode.MEMBER_NOT_FOUND));
+        // DB에서 사용자 정보 조회 후 반환
+        return memberRepository.findById(memberId);
+    }
+
+    @Override
+    public Member getAuthUser() {
+        return getAuthUserOptional()
+                .orElseThrow(() -> new IllegalStateException("유저 정보를 확인할 수 없습니다."));
     }
 }
