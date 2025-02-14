bb337110262e48087a56e2ba498462491fbc728d
buck <take6948@naver.com>
2025-02-14 17:31:06 +0900

    :sparkles: 댓글 검색 및 조회 관련 응답/요청 dto 추가 #267

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/dto/TopicCommentDto.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/dto/TopicCommentDto.java
@@ -1,11 +1,11 @@
 package com.buck.vsplay.domain.vstopic.dto;
 
 
+import com.buck.vsplay.global.dto.Pagination;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TopicCommentDto {
@@ -15,6 +15,30 @@ public class TopicCommentDto {
     public static class CommentCreateRequest{
         @NotNull(message = "작성할 코멘트가 비었습니다.")
         String content;
+    }
+
+    @Getter
+    @Setter
+    public static class CommentSearchRequest{
+        String keyword;
+        Integer page;
+        Integer size;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class Comment{
+        String content;
+        String createdAt;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class CommentSearchResponse{
+        List<Comment> commentList;
+        Pagination pagination;
     }
 
 }
