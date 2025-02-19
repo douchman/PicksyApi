6ae6e2fb3d47b1d8c7bb56f0ced5adaea09485ea
buck <take6948@naver.com>
2025-02-19 13:39:09 +0900

    :sparkles: 회원등록 요청 dto 추가 #301

--- vsplay/src/main/java/com/buck/vsplay/domain/member/dto/MemberDto.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/member/dto/MemberDto.java
@@ -13,6 +13,19 @@ import java.util.List;
 public class MemberDto {
 
     @Data
+    public static class CreateMemberRequest{
+        @NotNull(message = "아이디는 필수 입력 항목 입니다.")
+        private String loginId;
+
+        @NotNull(message = "이름이 비었습니다.")
+        @Size(min = 3, max = 50, message = "이름은 2자이상 50자 이하여야 합니다.")
+        private String memberName;
+
+        @NotNull(message = "비밀번호가 비었습니다.")
+        private String password;
+    }
+
+    @Data
     public static class MemberInfo{
 
         private Long id;
