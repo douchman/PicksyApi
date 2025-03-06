5e4e8c798c992f08da4d31d3bb2e5d7312392c75
buck <take6948@naver.com>
2025-03-06 14:15:25 +0900

    :wrench: allowedOrigin 설정 변경 #317

--- vsplay/src/main/java/com/buck/vsplay/global/security/configuration/SecurityConfig.java
+++ vsplay/src/main/java/com/buck/vsplay/global/security/configuration/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(
     ) {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("http://localhost:8081");
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
