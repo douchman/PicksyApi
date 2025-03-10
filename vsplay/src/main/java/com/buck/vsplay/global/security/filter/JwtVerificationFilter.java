9f109e68f3ffaae3548a565da3206bb166558284
buck <take6948@naver.com>
2025-03-10 18:32:47 +0900

    :sparkles: 토큰 검증 필터 수정 #320

--- vsplay/src/main/java/com/buck/vsplay/global/security/filter/JwtVerificationFilter.java
+++ vsplay/src/main/java/com/buck/vsplay/global/security/filter/JwtVerificationFilter.java
@@ -1,18 +1,16 @@
 package com.buck.vsplay.global.security.filter;
 
 import com.buck.vsplay.global.security.jwt.JwtService;
-import com.buck.vsplay.global.security.jwt.exception.JwtException;
-import com.buck.vsplay.global.security.jwt.exception.JwtExceptionHandler;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -22,32 +20,29 @@ import java.io.IOException;
 public class JwtVerificationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private final JwtExceptionHandler jwtExceptionHandler;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        // get jwt from cookie
+        String token = getJwtFromCookie(request);
+
+        if (token != null && jwtService.validateToken(token)) {
+            Authentication authentication = jwtService.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             filterChain.doFilter(request, response);
-            return ;
-        }
-
-        jwt = authHeader.substring(7);
-
-        try {
-            if(SecurityContextHolder.getContext().getAuthentication() == null && jwtService.validateToken(jwt)) {
-                UsernamePasswordAuthenticationToken authToken =
-                        new UsernamePasswordAuthenticationToken(jwtService.extractUserId(jwt), null, jwtService.extractAuthorities(jwt));
-
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-
-                filterChain.doFilter(request, response);
-            }
-        } catch (JwtException e){
-            jwtExceptionHandler.handleJwtException(response, e);
         }
     }
+
+    private String getJwtFromCookie(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("token".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
 }
