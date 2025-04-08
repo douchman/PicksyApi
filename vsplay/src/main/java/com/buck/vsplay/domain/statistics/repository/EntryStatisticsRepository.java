055a17dc339cd7dda8d5d083d244f096b3ef9d92
buck <take6948@naver.com>
2025-04-08 17:52:12 +0900

    :recycle: 엔트리 통계 조회 쿼리메서드 Pageable 로 변경 #351

--- vsplay/src/main/java/com/buck/vsplay/domain/statistics/repository/EntryStatisticsRepository.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/repository/EntryStatisticsRepository.java
@@ -1,6 +1,8 @@
 package com.buck.vsplay.domain.statistics.repository;
 
 import com.buck.vsplay.domain.statistics.entity.EntryStatistics;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,13 +11,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface EntryStatisticsRepository extends JpaRepository<EntryStatistics, Long>, JpaSpecificationExecutor<EntryStatistics> {
     @Query("SELECT es FROM EntryStatistics es WHERE es.topicEntry.id = :entryId")
     EntryStatistics findByEntryId(@Param("entryId") Long entryId);
 
     @EntityGraph(attributePaths = {"topicEntry"}) // for join fetch
-    List<EntryStatistics> findAll(Specification<EntryStatistics> specification);
+    Page<EntryStatistics> findAll(Specification<EntryStatistics> specification, Pageable pageable);
 }
