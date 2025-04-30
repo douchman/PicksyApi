6a204d70119d88bf62e0b4befe607461090d4291
buck <take6948@naver.com>
2025-04-30 13:54:13 +0900

    :sparkles: 엔트리 상성 통계 조회 쿼리메서드 수정 #389

--- vsplay/src/main/java/com/buck/vsplay/domain/statistics/repository/EntryVersusStatisticsRepository.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/repository/EntryVersusStatisticsRepository.java
@@ -1,7 +1,10 @@
 package com.buck.vsplay.domain.statistics.repository;
 
 import com.buck.vsplay.domain.statistics.entity.EntryVersusStatistics;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,11 +12,13 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface EntryVersusStatisticsRepository extends JpaRepository<EntryVersusStatistics, Long> {
+public interface EntryVersusStatisticsRepository extends JpaRepository<EntryVersusStatistics, Long>, JpaSpecificationExecutor<EntryVersusStatistics> {
 
     @Query("SELECT evs FROM EntryVersusStatistics evs WHERE evs.topicEntry.id = :entryId AND evs.opponentEntry.id = :opponentEntryId")
     EntryVersusStatistics findByEntryIdAndOpponentEntryId(@Param("entryId") Long entryId, @Param("opponentEntryId") Long opponentEntryId);
 
-    @Query("SELECT evs FROM EntryVersusStatistics evs JOIN FETCH evs.opponentEntry oe WHERE evs.topicEntry.id = :entryId")
-    List<EntryVersusStatistics> findWithOpponentEntryByEntryId(@Param("entryId") Long entryId);
+    @EntityGraph(attributePaths = {"opponentEntry"})
+    default List<EntryVersusStatistics> findWithOpponentEntryBySpecification(Specification<EntryVersusStatistics> specification){
+        return findAll(specification);
+    }
 }
