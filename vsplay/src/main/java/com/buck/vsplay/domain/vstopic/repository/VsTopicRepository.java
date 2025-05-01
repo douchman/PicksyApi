c027aa6824ce079edcca0fa6a0814b7ebf08ee61
buck <take6948@naver.com>
2025-05-01 16:09:44 +0900

    :recycle: 대결주제 조회 쿼리 메서드를 정렬기준에 따라 2개로 분리 #390

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/repository/VsTopicRepository.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/repository/VsTopicRepository.java
@@ -2,11 +2,14 @@ package com.buck.vsplay.domain.vstopic.repository;
 
 
 import com.buck.vsplay.domain.vstopic.entity.VsTopic;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface VsTopicRepository extends JpaRepository<VsTopic, Long>, JpaSpecificationExecutor<VsTopic> {
@@ -15,4 +18,24 @@ public interface VsTopicRepository extends JpaRepository<VsTopic, Long>, JpaSpec
     VsTopic findWithTournamentsByTopicId(@Param("topicId") Long topicId);
 
     VsTopic findWithTournamentsByShortCode(String shortCode);
+
+    @Query("""
+    SELECT vt FROM VsTopic vt
+    WHERE vt.visibility = 'PUBLIC'
+    AND vt.deleted = false
+    AND ( :title IS NULL OR :title = '' OR vt.title LIKE CONCAT('%',:title , '%'))
+    ORDER BY vt.createdAt DESC
+    """)
+    Page<VsTopic> findPublicTopicsByTitleOrderByNewest(@Param("title") String title, Pageable pageable);
+
+    @Query("""
+    SELECT vt FROM VsTopic vt
+    JOIN TopicStatistics ts
+    ON ts.vsTopic.id = vt.id
+    WHERE vt.visibility = 'PUBLIC'
+    AND vt.deleted = false
+    AND ( :title IS NULL OR :title = '' OR vt.title LIKE CONCAT('%',:title , '%'))
+    ORDER BY ts.totalMatches DESC
+    """)
+    Page<VsTopic> findPublicTopicsByTitleOrderByTotalMatches(@Param("title") String title, Pageable pageable);
 }
