a5f1e06d1c1e2b05739a4e27ebe3869f037e6330
buck <take6948@naver.com>
2025-05-21 18:51:16 +0900

    :recycle: 회원이 생성한 대결주제 조회 요청 Controller 신규 추가 #424 - 요청 URL 수정 - 기존 TopicController 로직을 그대로 사용

---
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/controller/MyTopicController.java
@@ -0,0 +1,26 @@
+package com.buck.vsplay.domain.vstopic.controller;
+
+
+import com.buck.vsplay.domain.vstopic.dto.VsTopicDto;
+import com.buck.vsplay.domain.vstopic.service.impl.VsTopicService;
+import com.buck.vsplay.global.dto.SingleResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("my")
+public class MyTopicController {
+    private final VsTopicService vsTopicService;
+
+    @GetMapping("topics")
+    public ResponseEntity<SingleResponseDto<VsTopicDto.VsTopicSearchResponse>> myVsTopics(
+            VsTopicDto.VsTopicSearchRequest vsTopicSearchRequest
+    ) {
+        return new ResponseEntity<>(new SingleResponseDto<>(HttpStatus.OK.value(), vsTopicService.getMyVsTopics(vsTopicSearchRequest)), HttpStatus.OK);
+    }
+}
