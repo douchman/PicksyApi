acb4f977c5910a7af23bd654783e078d75c1281b
buck <take6948@naver.com>
2025-05-26 16:04:40 +0900

    :recycle: 엔트리 조회 쿼리 메서드 삭제여부 조건 추가 #431

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/repository/EntryRepository.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/repository/EntryRepository.java
@@ -13,7 +13,7 @@ public interface EntryRepository extends JpaRepository<TopicEntry, Long> {
 
     List<TopicEntry> findByTopicIdAndDeletedFalse(Long topicId);
 
-    @Query("SELECT e FROM TopicEntry e JOIN FETCH e.topic WHERE e.id = :entryId")
+    @Query("SELECT e FROM TopicEntry e JOIN FETCH e.topic WHERE e.id = :entryId AND e.deleted = false")
     TopicEntry findWithTopicByEntryId(@Param("entryId") Long entryId);
 
     List<TopicEntry> findByTopicIdAndIdIn(Long topicId, List<Long> ids);
