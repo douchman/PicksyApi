ccaee0bb39204677e940131a4ad42026488430a4
buck <take6948@naver.com>
2025-06-04 18:53:21 +0900

    :fire: 불필요한 Gpt 관련 패키지 전체 제거 #465

--- vsplay/src/main/java/com/buck/vsplay/domain/gpt/exception/GptApiException.java
+++
@@ -1,9 +0,0 @@
-package com.buck.vsplay.domain.gpt.exception;
-
-import com.buck.vsplay.global.exception.BaseException;
-
-public class GptApiException extends BaseException {
-    public GptApiException(GptApiExceptionCode gptApiExceptionCode) {
-        super(gptApiExceptionCode);
-    }
-}
--- vsplay/src/main/java/com/buck/vsplay/domain/gpt/service/IGptService.java
+++
@@ -1,7 +0,0 @@
-package com.buck.vsplay.domain.gpt.service;
-
-import com.buck.vsplay.domain.gpt.dto.GptApiDto;
-
-public interface IGptService {
-    void moderateTextContent(GptApiDto.BadWordApiRequest badWordApiRequest);
-}
