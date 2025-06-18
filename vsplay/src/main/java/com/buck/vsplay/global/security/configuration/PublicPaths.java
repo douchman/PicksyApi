d2fca062e367f4be7f8925d08806f87976501fea
buck <take6948@naver.com>
2025-06-18 13:17:50 +0900

    :wrench: security origin 및 public path 추가 #512

--- vsplay/src/main/java/com/buck/vsplay/global/security/configuration/PublicPaths.java
+++ vsplay/src/main/java/com/buck/vsplay/global/security/configuration/PublicPaths.java
@@ -26,7 +26,8 @@ public class PublicPaths {
             Map.entry("/topics/play-records/*/matches/*", Set.of(HttpMethod.PATCH)),
             Map.entry("/topics/*/entries", Set.of(HttpMethod.GET)),
             Map.entry("/topics/*/comments", Set.of(HttpMethod.GET, HttpMethod.POST)),
-            Map.entry("/statistics/**", Set.of(HttpMethod.GET))
+            Map.entry("/statistics/**", Set.of(HttpMethod.GET)),
+            Map.entry("/actuator/prometheus", Set.of(HttpMethod.GET))
     );
 
     public static final Map<String, Set<HttpMethod>> OPTIONAL_AUTH_ENDPOINTS = Map.ofEntries(
