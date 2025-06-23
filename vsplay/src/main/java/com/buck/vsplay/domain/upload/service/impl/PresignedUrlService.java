5f2bf30643ce8e118af0de32eaf706ed3f7d5d00
buck <take6948@naver.com>
2025-06-23 18:33:52 +0900

    :sparkles: presigned url 서비스 추가 #516 - presigned url 생성

---
+++ vsplay/src/main/java/com/buck/vsplay/domain/upload/service/impl/PresignedUrlService.java
@@ -0,0 +1,49 @@
+package com.buck.vsplay.domain.upload.service.impl;
+
+import com.buck.vsplay.domain.upload.dto.PresignedUrlDto;
+import com.buck.vsplay.domain.upload.service.IPresignedUrlService;
+import com.buck.vsplay.global.security.service.impl.AuthUserService;
+import com.buck.vsplay.global.util.aws.s3.S3Util;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Locale;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PresignedUrlService implements IPresignedUrlService {
+
+    private final S3Util s3Util;
+    private final AuthUserService authUserService;
+
+    @Override
+    public PresignedUrlDto.GeneratePresignedUrlResponse generateClientUploadUrl(PresignedUrlDto.GeneratePresignedUrlRequest generatePresignedUrlRequest) {
+        Long memberId = authUserService.getAuthUser().getId();
+
+        List<PresignedUrlDto.PresignedFile> presignedFiles = generatePresignedUrlRequest.getRequestFiles().stream()
+                .map( file -> {
+                    String objectKey = generateObjectKey(memberId, file.getOriginalFileName());
+                    String presignedUploadUrl = s3Util.generatePreSignedUploadUrl(
+                            objectKey,
+                            file.getContentType(),
+                            Duration.ofMinutes(3));
+
+                    return PresignedUrlDto.PresignedFile.builder()
+                            .objectKey(objectKey)
+                            .presignedUploadUrl(presignedUploadUrl)
+                            .build();
+                }).toList();
+
+        return PresignedUrlDto.GeneratePresignedUrlResponse.builder()
+                .presignedFiles(presignedFiles)
+                .build();
+    }
+
+    private String generateObjectKey(Long memberId, String originalFilename) {
+        return memberId + "/" + s3Util.generateRandomFileName() + s3Util.getFileExtension(originalFilename).toLowerCase(Locale.ROOT);
+    }
+}
