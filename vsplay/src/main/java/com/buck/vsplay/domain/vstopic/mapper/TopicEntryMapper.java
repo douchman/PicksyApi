9842b82d6134daba685f80ddaafd82bc9870c87e
buck <take6948@naver.com>
2025-06-26 17:31:43 +0900

    :fire: 미사용 매핑 메서드 제거 #518

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/mapper/TopicEntryMapper.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/mapper/TopicEntryMapper.java
@@ -18,13 +18,4 @@ public interface TopicEntryMapper {
     @Mapping(target = "thumbnail", expression = "java(s3Util.getUploadedObjectUrl(topicEntry.getThumbnail()))")
     EntryDto.Entry toEntryDtoFromEntityWithoutSignedMediaUrl(TopicEntry topicEntry, S3Util s3Util);
 
-    /**
-     * 엔티티 업데이트를 위해 DTO 의 값을 기존 Entry 엔티티에 적용
-     * Null 필드는 자동으로 제외
-     * @param updatedRequest  기존 엔트리에 업데이트 할 변경된 엔트리 값
-     * @param topicEntry 업데이트를 적용할 기존의 엔트리
-     */
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    void applyChangesToTopicEntry(EntryDto.UpdateEntryRequest updatedRequest, @MappingTarget TopicEntry topicEntry);
-
 }
