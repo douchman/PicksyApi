29b21208a94c72b4f21364d56ac3d780118a1ad2
buck <take6948@naver.com>
2025-06-30 17:33:48 +0900

    :sparkles: 대결 주제 통계 응답 dto 토너먼트 통계 필드 추가 #544

--- vsplay/src/main/java/com/buck/vsplay/domain/statistics/dto/TopicStatisticsDto.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/statistics/dto/TopicStatisticsDto.java
@@ -5,6 +5,8 @@ import com.buck.vsplay.domain.vstopic.dto.EntryDto;
 import com.buck.vsplay.domain.vstopic.dto.VsTopicDto;
 import lombok.*;
 
+import java.util.List;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TopicStatisticsDto {
 
@@ -25,6 +27,7 @@ public class TopicStatisticsDto {
     @Builder
     public static class TopicStatisticsResponse{
         VsTopicDto.VsTopic topic;
+        List<TournamentStatisticsDto.TournamentStatistics> tournamentStatistics;
         TopicStatistics topicStatistics;
     }
 }
