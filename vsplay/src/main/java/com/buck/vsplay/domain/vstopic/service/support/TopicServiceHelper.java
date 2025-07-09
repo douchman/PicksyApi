a276196862c47a5870b500cf8475a6046a71c06d
buck <take6948@naver.com>
2025-07-09 17:43:37 +0900

    :fire: 사용하지 않는 메서드 제거 #574

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/support/TopicServiceHelper.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/support/TopicServiceHelper.java
@@ -1,6 +1,5 @@
 package com.buck.vsplay.domain.vstopic.service.support;
 
-import com.buck.vsplay.global.constants.Visibility;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -9,9 +8,5 @@ public class TopicServiceHelper {
 
     public static boolean isThumbnailUpdated(String thumbnail){
         return thumbnail != null && !thumbnail.isEmpty();
-    }
-
-    public static boolean isMissingPasswordForProtectedTopic(Visibility visibility, String accessCode){
-        return visibility == Visibility.PASSWORD && (accessCode == null || accessCode.isBlank());
     }
 }
