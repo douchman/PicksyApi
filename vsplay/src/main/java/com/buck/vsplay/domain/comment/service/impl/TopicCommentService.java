065a8e88919bb44503f3757fa76c0405c6fc7149
buck <take6948@naver.com>
2025-07-10 18:40:35 +0900

    :recycle: 추가된 Mapper 및 쿼리 메서드를 적용하여 댓글 조회 비즈니스 개선 #587 - Specification 사용하지 않음

--- vsplay/src/main/java/com/buck/vsplay/domain/comment/service/impl/TopicCommentService.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/comment/service/impl/TopicCommentService.java
@@ -2,13 +2,13 @@ package com.buck.vsplay.domain.comment.service.impl;
 
 import com.buck.vsplay.domain.comment.dto.TopicCommentDto;
 import com.buck.vsplay.domain.comment.entity.TopicComment;
+import com.buck.vsplay.domain.comment.mapper.CommentMapper;
 import com.buck.vsplay.domain.comment.service.ITopicCommentService;
 import com.buck.vsplay.domain.vstopic.entity.VsTopic;
 import com.buck.vsplay.domain.vstopic.exception.vstopic.VsTopicException;
 import com.buck.vsplay.domain.vstopic.exception.vstopic.VsTopicExceptionCode;
 import com.buck.vsplay.domain.comment.repository.TopicCommentRepository;
 import com.buck.vsplay.domain.vstopic.repository.VsTopicRepository;
-import com.buck.vsplay.domain.comment.specification.TopicCommentSpecification;
 import com.buck.vsplay.domain.vstopic.service.finder.TopicFinder;
 import com.buck.vsplay.global.dto.Pagination;
 import com.buck.vsplay.global.security.service.impl.AuthUserService;
@@ -17,12 +17,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -34,6 +30,7 @@ public class TopicCommentService implements ITopicCommentService {
     private final VsTopicRepository topicRepository;
     private final TopicCommentRepository topicCommentRepository;
     private final TopicFinder topicFinder;
+    private final CommentMapper commentMapper;
 
     @Override
     public TopicCommentDto.CommentCreateResponse createTopicComment(Long topicId, TopicCommentDto.CommentCreateRequest commentCreateRequest) {
@@ -62,29 +59,15 @@ public class TopicCommentService implements ITopicCommentService {
 
         topicFinder.findExistingById(topicId);
 
-        Page<TopicComment> topicCommentPage = topicCommentRepository.findAll(
-                TopicCommentSpecification.withAllFilters(topicId, commentSearchRequest.getKeyword(), false),
-                PageRequest.of(page, commentSearchRequest.getSize(), Sort.by(Sort.Direction.DESC, "createdAt")));
-
-        List<TopicCommentDto.Comment> topicCommentList = new ArrayList<>();
-
-        // entity -> dto mapping
-        for (TopicComment topicComment : topicCommentPage.getContent()) {
-            topicCommentList.add(
-                    TopicCommentDto.Comment.builder()
-                            .author(topicComment.getAuthor())
-                            .content(topicComment.getContent())
-                            .createdAt(DateTimeUtil.formatDateToSting(topicComment.getCreatedAt())).build()
-            );
-        }
+        Page<TopicComment> topicCommentWithPage = topicCommentRepository.findCommentByTopicIdAndContentOrderByNewest(topicId, commentSearchRequest.getKeyword(), PageRequest.of(page, commentSearchRequest.getSize()));
 
         return TopicCommentDto.CommentSearchResponse.builder()
-                .commentList(topicCommentList)
+                .commentList(commentMapper.toDtoListFromEntityList(topicCommentWithPage.getContent()))
                 .pagination(Pagination.builder()
-                        .totalPages(topicCommentPage.getTotalPages())
-                        .totalItems(topicCommentPage.getTotalElements())
-                        .currentPage(topicCommentPage.getNumber() + 1)
-                        .pageSize(topicCommentPage.getSize())
+                        .totalPages(topicCommentWithPage.getTotalPages())
+                        .totalItems(topicCommentWithPage.getTotalElements())
+                        .currentPage(topicCommentWithPage.getNumber() + 1)
+                        .pageSize(topicCommentWithPage.getSize())
                         .build())
                 .build();
     }
