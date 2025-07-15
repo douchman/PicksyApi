2ad0326fe8b9ddcab0761deb43dce4df1089fea0
buck <take6948@naver.com>
2025-07-15 19:14:34 +0900

    :recycle: 요청 dto 타입 json 으로 변경 #593

--- vsplay/src/main/java/com/buck/vsplay/domain/contact/controller/UserContactController.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/contact/controller/UserContactController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,8 @@ public class UserContactController {
     private final IUserContactService userContactService;
 
     @PostMapping()
-    public ResponseEntity<SingleResponseDto<Integer>> createUserContact(UserContactDto.ContactCreateRequest request) {
+    public ResponseEntity<SingleResponseDto<Integer>> createUserContact(
+            @RequestBody  UserContactDto.ContactCreateRequest request) {
         userContactService.createUserContact(request);
         return new ResponseEntity<>(new SingleResponseDto<>(HttpStatus.OK.value()), HttpStatus.OK);
     }
