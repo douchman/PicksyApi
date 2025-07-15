9c860b1b3984925119819ed52a99e0854cced3ab
buck <take6948@naver.com>
2025-07-15 19:03:53 +0900

    :sparkles: 유저 문의 등록 요청 API 경로 추가 #593

---
+++ vsplay/src/main/java/com/buck/vsplay/domain/contact/controller/UserContactController.java
@@ -0,0 +1,26 @@
+package com.buck.vsplay.domain.contact.controller;
+
+
+import com.buck.vsplay.domain.contact.dto.UserContactDto;
+import com.buck.vsplay.domain.contact.service.IUserContactService;
+import com.buck.vsplay.global.dto.SingleResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("contact")
+public class UserContactController {
+
+    private final IUserContactService userContactService;
+
+    @PostMapping()
+    public ResponseEntity<SingleResponseDto<Integer>> createUserContact(UserContactDto.ContactCreateRequest request) {
+        userContactService.createUserContact(request);
+        return new ResponseEntity<>(new SingleResponseDto<>(HttpStatus.OK.value()), HttpStatus.OK);
+    }
+}
