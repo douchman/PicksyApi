9fe4b49501c2700733b4b465990cfecc09ba41c5
buck <take6948@naver.com>
2025-07-16 20:10:42 +0900

    :sparkles: 문의 등록 slack 알림 서비스 추가 #595

---
+++ vsplay/src/main/java/com/buck/vsplay/domain/contact/service/support/InquirySlack.java
@@ -0,0 +1,34 @@
+package com.buck.vsplay.domain.contact.service.support;
+
+import com.slack.api.Slack;
+import com.slack.api.webhook.Payload;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class InquirySlack {
+    private static final String WEB_HOOK_URL = "https://hooks.slack.com/services/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"; // 문의 채널 webHook
+
+
+    public void sendInquiryCreatedSlackMessage(String author, String email, String title, String content) throws IOException {
+
+        String inquiryCreatedMsgTemplate = """
+                📩 *[Picksy 문의 도착]*
+                *문의자 명:* %s
+                *문의자 이메일:* %s
+                *문의 제목:* %s
+                *문의 내용*
+                %s
+                """.formatted(author, email, title, content);
+
+
+        Slack slack = Slack.getInstance();
+        Payload payload = Payload.builder()
+                .text(inquiryCreatedMsgTemplate)
+                .build();
+
+        slack.send(WEB_HOOK_URL, payload);
+    }
+
+}
