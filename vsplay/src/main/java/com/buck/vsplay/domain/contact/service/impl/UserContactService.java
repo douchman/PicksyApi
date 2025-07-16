e96010f33c8791dd7e1d736ee7d40ffa66b47ee9
buck <take6948@naver.com>
2025-07-16 20:11:18 +0900

    :recycle: 문의 등록 시 slack 메시지 전송 #595

--- vsplay/src/main/java/com/buck/vsplay/domain/contact/service/impl/UserContactService.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/contact/service/impl/UserContactService.java
@@ -6,6 +6,7 @@ import com.buck.vsplay.domain.contact.mapper.UserContactMapper;
 import com.buck.vsplay.domain.contact.repository.UserContactRepository;
 import com.buck.vsplay.domain.contact.service.IUserContactService;
 import com.buck.vsplay.domain.contact.service.support.InquiryEmail;
+import com.buck.vsplay.domain.contact.service.support.InquirySlack;
 import com.buck.vsplay.global.util.request.ClientInfoExtractor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,7 @@ public class UserContactService implements IUserContactService {
     private final UserContactMapper userContactMapper;
     private final UserContactRepository userContactRepository;
     private final InquiryEmail inquiryEmail;
+    private final InquirySlack inquirySlack;
 
     @Override
     public void createUserContact(UserContactDto.ContactCreateRequest request) {
@@ -35,6 +37,13 @@ public class UserContactService implements IUserContactService {
             inquiryEmail.sendInquiryMail(request.getAuthor(), request.getEmail(), request.getTitle(), request.getContent());
         } catch (Exception e) {
             log.warn("문의 등록 알림 메일 전송에 실패했습니다.[ author: {}, email: {}] -{} ", request.getAuthor(), request.getEmail(), e.getMessage(), e);
+        }
+
+        // Slack 알림 메시지 전송
+        try {
+            inquirySlack.sendInquiryCreatedSlackMessage(request.getAuthor(), request.getEmail(), request.getTitle(), request.getContent());
+        } catch (Exception e) {
+            log.warn("문의 등록 Slack 메시지 전송에 실패했습니다.[ author: {}, email: {}] -{} ", request.getAuthor(), request.getEmail(), e.getMessage(), e);
         }
 
     }
