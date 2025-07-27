03805a15e68528a033badbb62bd4563d6367b9ce
buck <take6948@naver.com>
2025-07-27 16:07:27 +0900

    :recycle: 공지 유형 기본 값 설정 #629

--- vsplay/src/main/java/com/buck/vsplay/domain/notice/entity/Notice.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/notice/entity/Notice.java
@@ -4,7 +4,6 @@ import com.buck.vsplay.domain.notice.constants.NoticeType;
 import com.buck.vsplay.global.entity.Timestamp;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Comment;
 
 @NoArgsConstructor
@@ -22,11 +21,11 @@ public class Notice extends Timestamp {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "NOTICE_SEQ_GENERATOR")
     private Long id;
 
-    @ColumnDefault("'DEFAULT'")
+    @Builder.Default
     @Enumerated(EnumType.STRING)
     @Column(name = "notice_type")
     @Comment("공지 유형")
-    private NoticeType noticeType;
+    private NoticeType noticeType = NoticeType.DEFAULT;
 
     @Column(name = "title")
     @Comment("공지 사항 제목")
