aea9a7cebf386e726149ded28377cb8996cddde2
buck <take6948@naver.com>
2025-08-01 16:37:31 +0900

    :sparkles: 금칙어 사용 관련 예외코드 추가 #639

--- vsplay/src/main/java/com/buck/vsplay/domain/member/exception/MemberExceptionCode.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/member/exception/MemberExceptionCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 public enum MemberExceptionCode implements BaseExceptionCode {
 
     MEMBER_NOT_FOUND(404, "일치하는 회원을 찾을 수 없습니다.", "MEMBER_NOT_FOUND"),
-    MEMBER_DUPLICATE_ID (409, "이미 존재하는 아이디 입니다.", "MEMBER_DUPLICATE_ID");
+    MEMBER_DUPLICATE_ID (409, "이미 존재하는 아이디 입니다.", "MEMBER_DUPLICATE_ID"),
+    BANNED_ID_USED (400, "사용할 수 없는 아이디입니다..", "BANNED_ID_USED"),
+    BANNED_NICKNAME_USED (400, "사용할 수 없는 닉네임입니다.", "BANNED_NICKNAME_USED");
 
 
     private final int status;
