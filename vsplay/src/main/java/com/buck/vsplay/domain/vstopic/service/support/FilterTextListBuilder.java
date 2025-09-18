c2769cd37b89be295d25198328bd43f2059f5d79
buck <take6948@naver.com>
2025-09-18 17:10:12 +0900

    :recycle: 문자열 리스트 빌더 null-safe 리팩토링 #664

--- vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/support/FilterTextListBuilder.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/vstopic/service/support/FilterTextListBuilder.java
@@ -3,12 +3,20 @@ package com.buck.vsplay.domain.vstopic.service.support;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FilterTextListBuilder {
 
     public static List<String> buildStringList(String ... strings){
-        return List.of(strings);
+        if(strings == null || strings.length == 0){
+            return Collections.emptyList();
+        }
+        return Arrays.stream(strings)
+                .filter(Objects::nonNull)
+                .toList();
     }
 }
