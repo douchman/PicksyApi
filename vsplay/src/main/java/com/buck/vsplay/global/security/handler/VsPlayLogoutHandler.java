f8af874ad05c364de87e9e3c0733733791dd295a
buck <take6948@naver.com>
2025-09-29 01:12:58 +0900

    :recycle: 로그아웃 처리 시 쿠키 도메인 추가 #667

--- vsplay/src/main/java/com/buck/vsplay/global/security/handler/VsPlayLogoutHandler.java
+++ vsplay/src/main/java/com/buck/vsplay/global/security/handler/VsPlayLogoutHandler.java
@@ -2,6 +2,7 @@ package com.buck.vsplay.global.security.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
@@ -11,12 +12,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class VsPlayLogoutHandler implements LogoutHandler {
+
+    @Value("${app.cookie.domain:#{null}}")
+    String cookieDomain;
+
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         ResponseCookie jwtCookie = ResponseCookie.from("token", "")
                 .httpOnly(true)
                 .secure(false)
                 .path("/")
+                .domain(cookieDomain)
                 .sameSite("Lax")
                 .maxAge(0) // 즉시 만료
                 .build();
