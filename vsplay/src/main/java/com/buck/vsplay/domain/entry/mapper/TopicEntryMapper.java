7f3c3acbff5afb6b515b9b57c2ba8841188fecd6
buck <take6948@naver.com>
2025-10-31 21:14:29 +0900

    :sparkles: 엔트리 mediaUrl 변환 default 메서드 추가 및 entity to dto 매핑 추가 #673 - 새로 추가된 default 메서드로 Mapping 설정

--- vsplay/src/main/java/com/buck/vsplay/domain/entry/mapper/TopicEntryMapper.java
+++ vsplay/src/main/java/com/buck/vsplay/domain/entry/mapper/TopicEntryMapper.java
@@ -3,6 +3,7 @@ package com.buck.vsplay.domain.entry.mapper;
 
 import com.buck.vsplay.domain.entry.dto.EntryDto;
 import com.buck.vsplay.domain.entry.entiity.TopicEntry;
+import com.buck.vsplay.global.constants.MediaType;
 import com.buck.vsplay.global.util.aws.s3.S3Util;
 import org.mapstruct.*;
 
@@ -15,7 +16,22 @@ public interface TopicEntryMapper {
     @Mapping(target = "thumbnail", expression = "java(s3Util.getUploadedObjectUrl(topicEntry.getThumbnail()))")
     EntryDto.Entry toEntryDtoFromEntity(TopicEntry topicEntry, S3Util s3Util);
 
+
+    @Mapping(target = "mediaUrl", expression = "java(resolveMediaUrl(topicEntry, s3Util))")
+    @Mapping(target = "thumbnail", expression = "java(s3Util.getUploadedObjectUrl(topicEntry.getThumbnail()))")
+    EntryDto.Entry toEntryDtoFromEntryEntity(TopicEntry topicEntry, S3Util s3Util);
+
+
+
     @Mapping(target = "thumbnail", expression = "java(s3Util.getUploadedObjectUrl(topicEntry.getThumbnail()))")
     EntryDto.Entry toEntryDtoFromEntityWithoutSignedMediaUrl(TopicEntry topicEntry, S3Util s3Util);
 
+
+    default String resolveMediaUrl(TopicEntry topicEntry, S3Util s3Util) {
+        if(topicEntry.getMediaType() == MediaType.YOUTUBE){
+            return topicEntry.getMediaUrl();
+        }
+
+        return s3Util.getUploadedObjectUrl(topicEntry.getMediaUrl());
+    }
 }
